6beffc55450163f78ebdfeea4ff8da28db8902ff
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2020-05-24 00:12:51 +0100

    initial commit

---
+++ app/src/main/java/com/example/buildawntask/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.buildawntask
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.toolbar.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(toolbar)
+    }
+}
