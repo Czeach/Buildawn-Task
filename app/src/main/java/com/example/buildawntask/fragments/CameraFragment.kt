9758826ab5a9b7cba76eafd3b42d5d604a4b5834
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2020-05-24 16:09:56 +0100

    2 screens

--- app/src/main/java/com/example/buildawntask/fragments/CameraFragment.kt
+++ app/src/main/java/com/example/buildawntask/fragments/CameraFragment.kt
@@ -11,13 +11,14 @@ import com.example.buildawntask.R
 /**
  * A simple [Fragment] subclass.
  */
-class EmployeeDataFragment : Fragment() {
+class CameraFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_employee_data, container, false)
+        return inflater.inflate(R.layout.fragment_camera, container, false)
     }
+
 }
